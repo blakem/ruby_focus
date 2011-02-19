258b3d960509d8ee8d15797fa61a98dada256a2f
Blake Mills <blakem30@yahoo.com>
2011-02-18 17:47:54 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyFocus::Application.config.secret_token = '6ab122c6a8a7f5c8a884b340edaf3a5adc7361785aced90ed89b1945b2f7a35dbee60a860d053dcb65f08d42678a6147d5f720e8b88031e9776bccf9da4ada94'
