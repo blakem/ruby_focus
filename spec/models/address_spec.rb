7994f3fb6fa298786863ecc9c074d4bcce89a297
Blake Mills <blakem30@yahoo.com>
2011-02-22 19:50:13 -0800

    Fixed broken address test

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -12,19 +12,20 @@ describe Address do
     describe "Validations" do
       [:street, :city, :zip].each do |attr|
         it "should require a #{attr}" do
-          person = Person.new(@valid.merge({ attr => "" }))
-          person.should_not be_valid
-          person.errors[attr].should_not be_nil
+          address = Address.new(@valid.merge({ attr => "" }))
+          address.should_not be_valid
+          address.errors[attr].should_not be_nil
         end
       end
     end
 
-    it "should not require a country and default to USA" do
-      address = Address.new(@valid.merge({ :country => "" }))
-      address.should be_valid
-      address.save
-      address.reload
-      address.country.should == "USA"
+    describe "When country is missing" do
+      it "should not require a country and default to USA" do
+        address = Address.new(@valid.merge({ :country => "" }))
+        address.country.should be_blank
+        address.should be_valid
+        address.save
+        address.country.should == "USA"
+      end
     end
-
 end
